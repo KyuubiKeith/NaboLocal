66df011787467502962b05c51d604ad687a42a13
Keith Kyuubi <tensakyuubi5@gmail.com>
2018-07-09 18:35:24 +0300

    trial

---
+++ NaboUI/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using NaboUI.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System.Web.Mvc;
+
+namespace NaboUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _usermanager;
+        private readonly SignInManager<IdentityUser> _signinManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _usermanager = userManager;
+            _signinManager = signInManager;
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> Register(RegisterViewModel vm)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { Username = vm.Email, Email = vm.Email };
+                var result = await _usermanager.CreateAsync(user, vm.Password);
+
+                if (result.succeeded)
+                {
+
+                    await _signinManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(" ", error.Description);
+                    }
+                }
+                return View(vm);
+            }
+        }
+    }
+
+    internal class ApplicationUser
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ NaboUI/Models/registerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace NaboUI.Models
+{
+    public class RegisterViewModel
+    {
+        //attributes
+        [Required, EmailAddress, MaxLength(256), Display(Name = "Email Adress")]
+        public string Email { get; set; }
+
+        [Required, MinLength(6), MaxLength(50), DataType(DataType.Password), Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required, MinLength(6), MaxLength(50), DataType(DataType.Password), Display(Name = "ConfirmPassword") ]
+        [Compare("Password", ErrorMessage = "Passwords Do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
